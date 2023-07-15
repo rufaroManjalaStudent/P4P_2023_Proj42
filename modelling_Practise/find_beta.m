fae20b9c8bed1a74574333eb88ee7c02615f5ec8
Rufaro Manjala <rman429@aucklanduni.ac.nz>
2023-07-15 13:58:13 +1200

    switching inputs around

--- modelling_Practise/find_beta.m
+++ modelling_Practise/find_beta.m
@@ -1,4 +1,4 @@
-function [Cp]  = find_beta(lambda,beta) 
+function [Cp]  = find_beta(beta, lambda) 
 
 Cp = 0.5176*(116/(lambda+0.08*beta) - 4.06/(1+beta^3)-0.4*beta-5)*exp((-21/(lambda+0.08*beta)+0.735/(1+beta^3)+0.0068*lambda));
 
